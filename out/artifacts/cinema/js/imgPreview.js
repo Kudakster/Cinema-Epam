d169aad889844aeb12b164caf3fbe80254d02199
Kudakster <95761962+Kudakster@users.noreply.github.com>
2022-06-20 05:44:24 +0300

    Delete out/artifacts/cinema directory

--- out/artifacts/cinema/js/imgPreview.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $('#image').change(function(){
-        $("#frames").html('');
-        for (var i = 0; i < $(this)[0].files.length; i++) {
-            $("#frames").append('<img src="'+window.URL.createObjectURL(this.files[i])+'" width="400px" height="400px"/>');
-        }
-    });
-});

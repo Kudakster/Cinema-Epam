101fc9e7c2b6f3f802d0d4fa944ebd21a5de159b
Kudakster <95761962+Kudakster@users.noreply.github.com>
2022-06-20 05:43:30 +0300

    Delete target directory

--- target/cinema-1.0-SNAPSHOT/js/imgPreview.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $('#image').change(function(){
-        $("#frames").html('');
-        for (var i = 0; i < $(this)[0].files.length; i++) {
-            $("#frames").append('<img src="'+window.URL.createObjectURL(this.files[i])+'" width="300px" height="400px"/>');
-        }
-    });
-});
